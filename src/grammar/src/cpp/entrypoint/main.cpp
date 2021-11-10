21bd1f07beda1266df0da4168fbe8b9697a0aff5
Mariska (desktop) <m.m.j.ijpelaar@gmail.com>
2021-11-10 17:09:50 +0100

    added capitalization

--- src/grammar/src/cpp/entrypoint/main.cpp
+++ src/grammar/src/cpp/entrypoint/main.cpp
@@ -3,16 +3,13 @@
 #include <grammar.h>
 #include <syntaxvisitor.h>
 
+const char* tests[] = {"b", "caligraphic b", "b tilde", "bold b hat", "capital a", "bold capital a hat"};
+
 int main() {
-	std::string test1("b");
-	std::string test2("caligraphic b");
-	std::string test3("b tilde");
-	std::string test4("bold b hat");
 	Logger logger(std::cerr, std::cerr, std::cerr);
 	SyntaxVisitor vis(logger);
-	int parseResult = grammar::generate_from_string(test1, vis);
-	grammar::generate_from_string(test2, vis);
-	grammar::generate_from_string(test3, vis);
-	grammar::generate_from_string(test4, vis);
-	return parseResult;
+
+	for (const char* test : tests)
+		grammar::generate_from_string(test, vis);
+	return 0;
 }
