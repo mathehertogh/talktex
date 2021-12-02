7180daf857419d98b1f2615e9ae56c2214ffdfcd
Patrick Star (J.T.S. Ram) <j.t.s.ram@umail.leidenuniv.nl>
2021-12-02 13:25:35 +0100

    updated setup

--- setup-ubuntu.sh
+++ setup-ubuntu.sh
@@ -8,7 +8,7 @@ set -e # Quit on error
 cd -- "$(dirname -- "$(readlink -f -- "${BASH_SOURCE[0]}")")" >/dev/null
 
 
-sudo apt install flex bison libalsa-dev portaudio19-dev
+sudo apt install flex bison alsa-utils portaudio19-dev
 pip3 install meson ninja deepspeech pyaudio webcrtvad halo --user
 
 mkdir -p "$BUILD_DIR"
