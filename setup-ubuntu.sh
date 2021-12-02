6b020e1365a1825df5b21db3057ef578e835dcdb
Arthur van der Staaij <arthurvanderstaaij@gmail.com>
2021-12-02 13:45:53 +0100

    Added deepspeech model download to setup-ubuntu.sh

--- setup-ubuntu.sh
+++ setup-ubuntu.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 BUILD_DIR="build"
+MODEL_DIR="deepspeech-models"
 
 set -e # Quit on error
 
@@ -10,6 +11,9 @@ cd -- "$(dirname -- "$(readlink -f -- "${BASH_SOURCE[0]}")")" >/dev/null
 
 sudo apt install flex bison alsa-utils portaudio19-dev
 pip3 install meson ninja deepspeech pyaudio webrtcvad halo --user
+
+mkdir -p "$MODEL_DIR"
+wget https://github.com/mozilla/DeepSpeech/releases/download/v0.9.3/deepspeech-0.9.3-models.pbmm -O "$MODEL_DIR/deepspeech-0.9.3-models.pbmm"
 
 mkdir -p "$BUILD_DIR"
 cd "$BUILD_DIR"
