ccf1d504f6417f51e4a2b4586af13d08cb7400b0
J.T.S. Ram <s2040328@u0037863.vuw.leidenuniv.nl>
2021-12-10 01:46:50 +0100

    script to work on university computers

---
+++ setup-302_306.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+PKG_DIR="packages"
+COMPILER_DIR="src/compiler"
+COMPILER_BUILD_DIR="build"
+
+TALKTEX_DIR="src/talktex"
+TALKTEX_MODEL_DIR="deepspeech/models"
+
+####################################################################################################
+
+set -e # Quit on error
+script_dir=$(cd -- "$(dirname -- "$(readlink -f -- "${BASH_SOURCE[0]}")")" >/dev/null && pwd)
+
+####################################################################################################
+
+echo "$(tput bold)TalkTeX: Starting common setup...$(tput sgr0)"
+echo ""
+
+# Install required packages
+mkdir -p "$script_dir/$PKG_DIR/tmp"
+cd "$script_dir/$PKG_DIR/tmp"
+apt download libportaudio2
+apt download portaudio19-dev
+#apt download ninja-build
+for file in *.deb; do
+	dpkg -x $file ../
+done
+cd "$script_dir"
+rm -rf "$script_dir/$PKG_DIR/tmp"
+
+# Export to environment variables
+export "C_INCLUDE_PATH=$script_dir/$PKG_DIR/usr/include:$C_INCLUDE_PATH"
+export "LIBRARY_PATH=$script_dir/$PKG_DIR/usr/lib:$LIBRARY_PATH"
+export "LIBRARY_PATH=$script_dir/$PKG_DIR/usr/lib/x86_64-linux-gnu:$LIBRARY_PATH"
+export "LD_LIBRARY_PATH=$script_dir/$PKG_DIR/usr/lib:$LD_LIBRARY_PATH"
+export "LD_LIBRARY_PATH=$script_dir/$PKG_DIR/usr/lib/x86_64-linux-gnu:$LD_LIBRARY_PATH"
+export "PATH=$script_dir/$PKG_DIR/usr/bin:$PATH"
+echo $LD_LIBRARY_PATH
+
+# Install python libraries
+pip3 install meson ninja pyaudio deepspeech webrtcvad halo numpy scipy --user
+export "PATH=$HOME/.local/bin:$PATH"
+#export "NINJA=~/.local/bin"
+echo $PATH
+
+# Pyaudio needs some special treatment
+
+# Setup compiler
+cd "$script_dir/$COMPILER_DIR"
+        mkdir -p "$COMPILER_BUILD_DIR"
+        cd "$COMPILER_BUILD_DIR"
+        meson
+
+# Setup talktex
+cd "$script_dir/$TALKTEX_DIR"
+        mkdir -p "$TALKTEX_MODEL_DIR"
+        wget "https://github.com/mozilla/DeepSpeech/releases/download/v0.9.3/deepspeech-0.9.3-models.pbmm" -O "$TALKTEX_MODEL_DIR/deepspeech-0.9.3-models.pbmm"
+
+echo ""
+echo "$(tput bold; tput setaf 2)TalkTeX: Setup succesful!$(tput sgr0)"
+
+# compile talktex
+
